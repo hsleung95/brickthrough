6ae3c192a479153f9a75a47701bfdb4677c2b094
hsleung95 <honsing1995@gmail.com>
2026-04-25 20:32:58 +0100

    clamping paddle

--- paddle.gd
+++ paddle.gd
@@ -1,20 +1,23 @@
-extends CharacterBody2D
+extends Area2D
 
+var speed = 300.0
+var screen_size
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
 
+func _process(delta):
+	var velocity = calVelocity()
+	position += velocity * delta
+	position = position.clamp(screen_size * 0.01, screen_size * 0.85)
 
-func _physics_process(delta: float) -> void:
-	var direction = calDirection()
-	velocity.x = direction * SPEED
-
-	move_and_slide()
-
-func calDirection():
+func calVelocity():
+	var velocity = Vector2.ZERO
 	if (Input.is_action_pressed("move_left")):
-		return -1
+		velocity.x += -1
 	elif (Input.is_action_pressed("move_right")):
-		return 1
-	else:
-		return 0
+		velocity.x += 1
+	if (velocity.length() > 0):
+		velocity = velocity.normalized() * speed
+	return velocity
